8c031b88f4470dd520fff30bf2ed1dbbf803d781
Zach Ayers <ayers.dev@icloud.com>
2020-11-21 18:40:13 -0600

    fixes css namespace clashing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,26 @@
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingModule from "./components/MarketingModule";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   return (
-    <div>
-      <MarketingModule />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingModule />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
